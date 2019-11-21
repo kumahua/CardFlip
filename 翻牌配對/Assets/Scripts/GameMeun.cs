8170ec58347f9ed397be5f5691b574c23baad965
kumahua <40682280+kumahua@users.noreply.github.com>
2019-11-21 12:16:00 +0800

    1121

---
+++ 翻牌配對/Assets/Scripts/GameMeun.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMeun : MonoBehaviour {
+
+	public void Level1Game() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+	}
+
+	public void Level2Game() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+2);
+	}
+
+	public void Return() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1);
+	}
+}
